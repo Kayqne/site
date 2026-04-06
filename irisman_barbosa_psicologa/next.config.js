7f71b4ec20c5443f70872afa1464e4041b97f62d
Kayque Lopes <73949134+Kayqne@users.noreply.github.com>
2026-04-06 11:30:43 -0300

    Update next.config.js

--- irisman_barbosa_psicologa/next.config.js
+++ irisman_barbosa_psicologa/next.config.js
@@ -1,19 +1,10 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  // Mantive sua saída, mas se der erro de 'standalone', você pode comentar a linha abaixo
-  output: process.env.NEXT_OUTPUT_MODE, 
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
-    ignoreDuringBuilds: true, // Já estava true, OK
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    // MUDANÇA AQUI: Mudei para TRUE para o build não cancelar nos tipos
-    ignoreBuildErrors: true, 
+    ignoreBuildErrors: true,
   },
   images: { unoptimized: true },
 };
