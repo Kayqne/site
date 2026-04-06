2928a474e4037da167b9d3b512680e0d01188ee0
Kayque Lopes <73949134+Kayqne@users.noreply.github.com>
2026-04-06 20:41:52 -0300

    Update page.tsx

--- irisman_barbosa_psicologa/app/admin/page/page.tsx
+++ irisman_barbosa_psicologa/app/admin/page/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import EditSiteConfigForm from '../components/EditSiteConfigForm';
 import { prisma } from '@/lib/db';
