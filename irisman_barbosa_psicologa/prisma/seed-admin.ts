d009ade0547ec9704684a629f6ee46ce955ab825
Kayque Lopes <73949134+Kayqne@users.noreply.github.com>
2026-04-06 20:20:29 -0300

    Update seed-admin.ts

--- irisman_barbosa_psicologa/prisma/seed-admin.ts
+++ irisman_barbosa_psicologa/prisma/seed-admin.ts
@@ -1,1 +1,32 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
 
+const prisma = new PrismaClient();
+
+async function main() {
+  const hashedPassword = await bcrypt.hash('SUA_SENHA_AQUI', 10);
+  
+  const admin = await prisma.user.upsert({
+    where: { email: 'seu-email@exemplo.com' },
+    update: {
+      role: 'admin'
+    },
+    create: {
+      email: 'seu-email@exemplo.com',
+      name: 'Admin Irisman',
+      password: hashedPassword,
+      role: 'admin',
+    },
+  });
+
+  console.log('Usuário admin criado/atualizado:', admin);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
